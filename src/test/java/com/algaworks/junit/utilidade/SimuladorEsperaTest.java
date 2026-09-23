3e0bf7c20b616d14218f83699c1ee06f725f9f53
leticia-bezerra <leticia21bezerradearaujo@gmail.com>
2026-09-23 10:42:38 -0300

    feat: Assertions timeout

---
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -0,0 +1,18 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+class SimuladorEsperaTest {
+
+    @Test
+    void esperar() {
+    }
+
+    @Test
+    public void deveEsperarENaoDarTimeout() {
+        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofMillis(10)));
+    }
+}
