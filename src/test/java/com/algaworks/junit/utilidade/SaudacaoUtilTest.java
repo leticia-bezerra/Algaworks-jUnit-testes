96fa1634794c961f79593467d7eed47cf59f1179
leticia-bezerra <leticia21bezerradearaujo@gmail.com>
2026-09-23 11:48:45 -0300

    feat:cobrir condicoes Saudacao no test Saudar

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -8,8 +8,14 @@ class SaudacaoUtilTest {
 
     @Test
     public void saudar() {
-       String saudar = SaudacaoUtil.saudar(22);
-        assertEquals("Boa noite", saudar, "Saudacao incorreta");
+        String saudarDia = SaudacaoUtil.saudar(9);
+        String saudarTarde = SaudacaoUtil.saudar(13);
+        String saudarNoite = SaudacaoUtil.saudar(22);
+        assertAll("Assercoes de pessoa",
+        () -> assertEquals("Bom dia", saudarDia, "Saudacao incorreta"),
+        () -> assertEquals("Boa tarde", saudarTarde, "Saudacao incorreta"),
+        () -> assertEquals("Boa noite", saudarNoite, "Saudacao incorreta")
+        );
     }
 
     @Test
