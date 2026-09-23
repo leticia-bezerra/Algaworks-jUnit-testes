3df93621ae9a9ec828b85cca424937ad81504e91
leticia-bezerra <leticia21bezerradearaujo@gmail.com>
2026-09-23 11:08:44 -0300

    feat: Ignorando testes condicionamente com Assumptions

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -1,8 +1,11 @@
 package com.algaworks.junit.utilidade;
 
+import jdk.jfr.Enabled;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import java.time.Duration;
 
@@ -14,7 +17,9 @@ class SimuladorEsperaTest {
     }
 
     @Test
+    @EnabledIfEnvironmentVariable(named = "ENV", matches = "DEV")
     public void deveEsperarENaoDarTimeout() {
+        //Assumptions.assumeTrue("PROD".equals(System.getProperty("ENV")), () -> "Teste ignorado, nao deve ser executado em PRODUÇAO");
         Assertions.assertTimeoutPreemptively(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofMillis(10)));
     }
 }
